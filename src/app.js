73ecd805c628226eb80e4141109a5565dc5ef165
santhoshmp89 <santhoshmp89@gmail.com>
2019-10-14 23:43:55 +0530

    Webpack configuration with dev-server and many presets

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const TextField = () => <p>Test React App</p>;
+const TextField = () => <p>Test React App dev-server</p>;
 
 ReactDOM.render (<TextField />, document.getElementById ('root'));
 
 console.log ('hi', process.env.NODE_ENV);
+
+class OldSyntax {
+  constructor () {
+    this.name = 'Mike';
+  }
+}
+
+var oldSyntax = new OldSyntax ();
+console.log (oldSyntax);
+
+class NewSyntax {
+  name = 'Sam';
+}
+var newSyntax = new NewSyntax ();
+console.log (newSyntax);
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,29 @@
 const path = require ('path');
-const isProd = process.env.NODE_ENV === 'production'
-  ? 'production'
-  : 'development';
-console.log (isProd);
-module.exports = {
-  mode: isProd,
-  entry: './src/app.js',
-  output: {
-    path: path.resolve (__dirname, './public/dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+// const isProd = process.env.NODE_ENV === 'production'
+//   ? 'production'
+//   : 'development';
+
+module.exports = env => {
+  return {
+    mode: env.NODE_ENV,
+    entry: './src/app.js',
+    output: {
+      path: path.resolve (__dirname, './public/dist'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-      },
-    ],
-  },
+      ],
+    },
+    devServer: {
+      contentBase: path.join (__dirname, 'public'),
+    },
+  };
 };
